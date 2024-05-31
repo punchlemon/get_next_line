0666f9e9f0af660ffdac94680861b5a68f385c49
punchlemon <tsukiwahigashini0410@gmail.com>
2024-05-31 20:33:56 +0900

    Modified header

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: retanaka <retanaka@student.42.tokyo>       +#+  +:+       +#+        */
+/*   By: retanaka <retanaka@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 08:56:18 by retanaka          #+#    #+#             */
-/*   Updated: 2024/05/27 19:16:10 by retanaka         ###   ########.fr       */
+/*   Updated: 2024/05/31 20:33:41 by retanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
